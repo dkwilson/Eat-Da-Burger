362eaccf250aacab8229b75c1fd9abe0328ee1e7
dkwilson <general@ytechnology.net>
2019-03-03 20:30:22 -0500

    first push/heroku

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ("Cheeseburger", 1);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Doubleburger", 1);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Bacon Burger", 0);
---
+++ models/burger.js
@@ -0,0 +1,23 @@
+var orm = require("../config/orm.js");
+
+var burger = {
+    selectAll: function(cb) {
+    orm.selectAll("burger_name", function(res) {
+      cb(res);
+    });
+  },
+  // The variables cols and vals are arrays.
+  insertOne: function(cols, vals, cb) {
+    orm.insertOne("burger_names", cols, vals, function(res) {
+      cb(res);
+    });
+  },
+  updateOne: function(objColVals, condition, cb) {
+    orm.updateOne("burger_name", objColVals, condition, function(res) {
+      cb(res);
+    });
+  }
+};
+
+// Export the database functions for the controller (catsController.js).
+module.exports = burger;
